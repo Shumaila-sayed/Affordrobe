34a82666ed96c5071907e44694efa511130736e3
Shumaila <sayedshumaila27@gmail.com>
2025-04-29 22:02:26 +0530

    Use tailwind

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,78 @@
+import { NavLink } from "react-router";
+
 const Navbar = () => {
     return (
-      <h1>hiee</h1>  
-    )
+			<header>
+				<NavLink
+					to='/'
+				>
+					Logo
+				</NavLink>
+				<nav>
+					<ul>
+						<li>
+							<NavLink
+								to='/'
+								className={({ isActive }) =>
+									isActive
+										? 'text-blue-600 font-semibold border-b-2 border-blue-600'
+										: 'text-gray-600 hover:text-blue-500'
+								}
+							>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								to='/products'
+								className={({ isActive }) =>
+									isActive
+										? 'text-blue-600 font-semibold border-b-2 border-blue-600'
+										: 'text-gray-600 hover:text-blue-500'
+								}
+							>
+								Shop
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+				<ul>
+					<li>
+						<NavLink
+							to='/cart'
+							className={({ isActive }) =>
+								isActive
+									? 'text-blue-600 font-semibold border-b-2 border-blue-600'
+									: 'text-gray-600 hover:text-blue-500'
+							}
+						>
+							Cart
+						</NavLink>
+					</li>
+				</ul>
+			</header>
+		);
 } 
 
 export default Navbar;
+
+/* 
+something about how to share product information between shop and cart
+
+ Passing data
+import { Link } from 'react-router-dom';
+
+<Link to={{ pathname: "/newPage", state: { myValue: "example" } }}>
+  Go to New Page
+</Link>
+
+
+// Receiving data
+import { useLocation } from 'react-router-dom';
+
+const NewPage = () => {
+  const location = useLocation();
+  const value = location.state.myValue;
+
+  return <div>{value}</div>;
+};  */
