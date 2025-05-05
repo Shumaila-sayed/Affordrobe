f5604f96b153cb5a344c5ee71f4db0c4cd2585df
Shumaila <sayedshumaila27@gmail.com>
2025-05-05 13:52:50 +0530

    Include loading and error states

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,27 +1,38 @@
-import { useState, useEffect } from "react";
-import ProductCard from "../Components/ProductCard";
+import { useState, useEffect } from 'react';
+import ProductCard from '../Components/ProductCard';
 
 const Products = ({ cartList, setCartList }) => {
 	const [productList, setProductList] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
 
 	const fetchProducts = async () => {
+		setIsLoading(true);
+		setErrorMessage('');
+
 		try {
 			const response = await fetch('https://fakestoreapi.com/products');
 
 			if (!response.ok) {
-				throw new Error('Failed to fetch characters');
+				throw new Error('Failed to fetch products');
 			}
 
 			const data = await response.json();
 			if (data.Response === 'False') {
+				setErrorMessage(data.Error || 'Failed to fetch products.');
+				setProductList([]);
 				return;
 			}
 
 			if (data) {
 				setProductList(data);
+				setIsLoading(false);
 			}
 		} catch (error) {
-			console.error(`Error fetching characters: ${error}`);
+			console.error(`Error fetching products: ${error}`);
+			setErrorMessage('Error fetching products. Please try again later.');
+		} finally {
+			setIsLoading(false);
 		}
 	};
 
@@ -33,16 +44,26 @@ const Products = ({ cartList, setCartList }) => {
 		<>
 			<h1>Find Your Style</h1>
 
-			{productList.map((product) => (
-				<ProductCard
-					product={product}
-					cartList={cartList}
-					setCartList={setCartList}
-					key={product.id}
-				/>
-			))}
+			{isLoading ? (
+				<h1>Loading...</h1>
+			) : errorMessage ? (
+				<p className='text-red-800 text-center mt-15 text-2xl'>
+					{errorMessage}
+				</p>
+			) : (
+				<div>
+					{productList.map((product) => (
+						<ProductCard
+							product={product}
+							cartList={cartList}
+							setCartList={setCartList}
+							key={product.id}
+						/>
+					))}
+				</div>
+			)}
 		</>
 	);
 };
 
-export default Products
+export default Products;
