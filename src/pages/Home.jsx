f6154004750d9d78e3336dcf7ce2f32a9334ae9a
Shumaila <sayedshumaila27@gmail.com>
2025-05-05 13:54:02 +0530

    include start of home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
-const Home = () => {
-    return (
-        <h1>This is home</h1>
-    )
-}
+import { NavLink } from 'react-router-dom';
 
-export default Home
+const Home = () => {
+	return (
+		<div>
+			<h1>This is home</h1>
+
+			<NavLink to='/products'>
+				<button>Discover Your Style</button>
+			</NavLink>
+		</div>
+	);
+};
+
+export default Home;
