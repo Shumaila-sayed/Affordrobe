8aa339e1102295977a7d9c6b3d9656786538bbf9
Shumaila <sayedshumaila27@gmail.com>
2025-05-07 01:59:34 +0530

    give content to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,37 @@ import { NavLink } from 'react-router-dom';
 
 const Home = () => {
 	return (
-		<div>
-			<h1>This is home</h1>
+		<>
+			<div>
+			<h1>Find Products That Matches Your Style</h1>
+			<p>
+				Browse through our diverse range of meticulously crafted garments,
+				designed to bring out your individuality and cater to your sense of
+				style.
+			</p>
 
 			<NavLink to='/products'>
-				<button>Discover Your Style</button>
+				<button>Shop Now</button>
 			</NavLink>
-		</div>
+
+			<div>
+				<p>200+</p>
+				<p>International Brands</p>
+			</div>
+			<div>
+				<p>2,000+</p>
+				<p>High-Quality Products</p>
+			</div>
+			<div>
+				<p>35,000+</p>
+				<p>Happy Customers</p>
+			</div>
+			</div>
+			<div>
+				<img src="" alt="" />
+			</div>
+		</>
+		
 	);
 };
 
