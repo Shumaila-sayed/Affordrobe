260bcc53e76b2db1060ca7105450c288abdf1ac7
Shumaila <sayedshumaila27@gmail.com>
2025-05-07 20:22:15 +0530

    Style home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,37 +2,46 @@ import { NavLink } from 'react-router-dom';
 
 const Home = () => {
 	return (
-		<>
-			<div>
-			<h1>Find Products That Matches Your Style</h1>
-			<p>
-				Browse through our diverse range of meticulously crafted garments,
-				designed to bring out your individuality and cater to your sense of
-				style.
-			</p>
+		<div className='lg:flex bg-grey lg:justify-between '>
+			<div className='  text-center text-black-50 lg:pt-20 pt-16 font-cal'>
+				<h1 className='text-5xl font-bold lg:px-8 tracking-wide'>
+					FIND PRODUCTS THAT MATCHES YOUR STYLE
+				</h1>
+				<p className='pt-4 lg:px-8 lg:mt-4'>
+					Browse through our diverse range of meticulously crafted garments,
+					designed to bring out your individuality and cater to your sense of
+					style.
+				</p>
 
-			<NavLink to='/products'>
-				<button>Shop Now</button>
-			</NavLink>
+				<NavLink to='/products'>
+					<button className='bg-peach rounded-full mt-8 px-8 py-1.5 text-amber-50 cursor-pointer hover:bg-coral-red lg:mt-12'>
+						Shop Now
+					</button>
+				</NavLink>
 
-			<div>
-				<p>200+</p>
-				<p>International Brands</p>
+				<div className='flex gap-4 justify-center mt-12 lg:mt-18'>
+					<div className='border-r-black/50 border-r-2 pr-2'>
+						<p className='text-left text-3xl'>200+</p>
+						<p>International Brands</p>
+					</div>
+					<div className='border-r-black/50 border-r-2 pr-2'>
+						<p className='text-left text-3xl'>2,000+</p>
+						<p>High-Quality Products</p>
+					</div>
+					<div>
+						<p className='text-left text-3xl'>35,000+</p>
+						<p>Happy Customers</p>
+					</div>
+				</div>
 			</div>
-			<div>
-				<p>2,000+</p>
-				<p>High-Quality Products</p>
+			<div className=''>
+				<img
+					className='lg:h-[650px] lg:w-[800px]'
+					src='/hero-img1.jpg'
+					alt='hero image'
+				/>
 			</div>
-			<div>
-				<p>35,000+</p>
-				<p>Happy Customers</p>
-			</div>
-			</div>
-			<div>
-				<img src="" alt="" />
-			</div>
-		</>
-		
+		</div>
 	);
 };
 
